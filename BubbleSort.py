084c547dd9b5e993a5433fcc511a0a4b0f709b23
Nicht-menschlich <j.35ul.54@gmail.com>
2021-01-16 13:53:49 +0100

    Updated to OOP

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,47 +1,18 @@
 import time as t
+from Utils.Utils import Utils as u
 #This is just a method that animates the text and makes a new line after
-def printProg(time, text):
-    for i in list(text):
-        print(i, end='')
-        t.sleep(time)
-    print()
 
+sortList = u.askForIntInput()
 #first the input of the list to be sorted happens
-printProg(0.05, "You will be asked to insert numbers")
+u.printProg(0.05, "You will be asked to insert numbers")
 t.sleep(0.75)
-printProg(0.05, "There it is possible to insert them seperately or in one piece seperating them with a ','!")
+u.printProg(0.05, "There it is possible to insert them seperately or in one piece seperating them with a ','!")
 t.sleep(0.75)
-printProg(0.05, "Have fun with my program!")
+u.printProg(0.05, "Have fun with my program!")
 t.sleep(0.5)
-sortList = []
-while sortList == []:
-    printProg(0.05, "Please insert a the list/items: ")
-    inStr = input()
-    if ',' in inStr:
-        currentInt = "" #sets value for currentInt
-        for i in range(len(inStr)):
-            char = list(inStr)[i]
-            if char != ' ':
-                if char == ',' and currentInt != "":
-                    try:
-                        sortList += [int(currentInt)]   #adds the new number to the list
-                        currentInt = "" #resets the value of currentInt
-                    except:
-                        printProg(0.05, "The input contains unexpected characters!")
-                        t.sleep(0.5)
-                        sortList = []   	#resets tha value of the list
-                        break
-                else:
-                    currentInt += char  #adds the current character of the string input to the currentInt string
-                    if i == len(inStr)-1:
-                        try:
-                            sortList += [int(currentInt)]   #adds the new number to the list
-                        except:
-                            printProg(0.05, "The input contains unexpected characters!")
-                            t.sleep(0.5)
-                            sortList = []   	#resets tha value of the list
-printProg(0.05, "Your input is ready to sort now. Below you can see it")
-printProg(0.05, str(sortList))
+
+u.printProg(0.05, "Your input is ready to sort now. Below you can see it")
+u.printProg(0.05, str(sortList))
 t.sleep(0.5)
 print("Work in Progress", end='')
 t.sleep(0.5)
@@ -59,5 +30,5 @@ while isSorted == False:
             sortList[i] = item2
             sortList[i+1] = item1
             isSorted = False    #tells the algorithm that it might still be unsorted
-printProg(0.05, "Your List has been sorted with Bubble sort. You're welcome")
-printProg(0.05, str(sortList))
+u.printProg(0.05, "Your List has been sorted with Bubble sort. You're welcome")
+i.printProg(0.05, str(sortList))
--- Utils/Utils.py
+++ Utils/Utils.py
@@ -1,1 +1,37 @@
+import time as t
 class Utils:
+    def askForIntInput(self):
+        sortList = []
+        while sortList == []:
+            self.printProg(0.05, "Please insert a the list/items: ")
+            inStr = input()
+            if ',' in inStr:
+                currentInt = ""  # sets value for currentInt
+                for i in range(len(inStr)):
+                    char = list(inStr)[i]
+                    if char != ' ':
+                        if char == ',' and currentInt != "":
+                            try:
+                                sortList += [int(currentInt)]  # adds the new number to the list
+                                currentInt = ""  # resets the value of currentInt
+                            except:
+                                self.printProg(0.05, "The input contains unexpected characters!")
+                                t.sleep(0.5)
+                                sortList = []  # resets tha value of the list
+                                break
+                        else:
+                            currentInt += char  # adds the current character of the string input to the currentInt string
+                            if i == len(inStr) - 1:
+                                try:
+                                    sortList += [int(currentInt)]  # adds the new number to the list
+                                except:
+                                    self.printProg(0.05, "The input contains unexpected characters!")
+                                    t.sleep(0.5)
+                                    sortList = []  # resets tha value of the list
+        return sortList
+
+    def printProg(self, time, text):
+        for i in list(text):
+            print(i, end='')
+            t.sleep(time)
+            print("")
